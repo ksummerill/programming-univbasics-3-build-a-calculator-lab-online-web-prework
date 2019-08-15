6ef6ab64850c281db96d588234a701dceeaa4fd7
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-15 21:40:25 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
+p addition(5, 4)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+p subtraction(10,5)
 
 def division(num1, num2)
-
+  num1 / num2
 end
+p division(50,2)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+p multiplication(4,30)
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+p modulo(34,5)
+
 
 def square_root(num)
-
+  math.sqrt(81)
 end
+p square_root
